d465e547daa52c9e9fe3868b97617dd6e2121763
AbhishekChamp <abhishek96.r@gmail.com>
2022-09-15 00:00:47 +0530

    04_Array_List

---
+++ 04_Array_List/01_avg_temp.py
@@ -0,0 +1,10 @@
+# Calculating Average temperature
+
+numDays = int(input("How many day's temperature? "))
+total = 0
+for i in range(1, numDays+1):
+    nextDay = int(input("Day " + str(i) + "'s high temp: "))
+    total += nextDay
+
+avg = round(total / numDays,2)
+print("\nAverage= " + str(avg))
